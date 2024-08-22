b4df623622e6bbccaffeed4d0c0b8c2edd2bb9fb
Dulitha <47850462+dulithaks@users.noreply.github.com>
2024-08-22 09:20:57 +0000

    Some modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 function App() {
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="Star Gazers" />
+          <h1>Meet the <i style={{color: "SteelBlue"}}>StarGazers</i></h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={ () => alert('Clicked') }>Click Me</button>
+        </hgroup>
+      </article>
+    </div>
   )
 }
 export default App
